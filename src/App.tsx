174a99668e39723287a09c8058ad0e207e1aba6e
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-03-28 13:11:18 +0300

    add bg image

--- src/App.tsx
+++ src/App.tsx
@@ -140,8 +140,6 @@ export const App: React.FC = () => {
 
   return (
     <div className="todoapp">
-      <h1 className="todoapp__title">todos</h1>
-
       <div className="todoapp__content">
         <Header
           addTodoOnServer={addTodoOnServer}
